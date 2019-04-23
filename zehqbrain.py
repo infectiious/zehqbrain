68c44e9d42938fe8cd6f9ac358cb88360aa3e019
Infectiious <kirkwoodjoshua@gmail.com>
2019-04-23 16:47:25 +1000

    initial commit

---
+++ zehqbrain.py
@@ -0,0 +1,20 @@
+import narcissisticpersonality
+import peanut
+import year7math
+
+myname = ("Liam")
+playingteams = ['Brisbane Broncos', 'Canberra Raiders', 'Canterbury-Bankstown Bulldogs', 'Cronulla-Sutherland Sharks', 'Gold Coast Titans', 'Manly Warringah Sea Eagles', 'Melbourne Storm', 'Newcastle Knights', 'New Zealand Warriors', 'North Queensland Cowboys', 'Parramatta Eels', 'Penrith Panthers', 'South Sydney Rabbitohs', 'St. George Illawarra Dragons', 'Sydney Roosters', 'Wests Tigers']
+mydiet = ("Horrible")
+
+def find_my_team():
+  for score in playing:
+    if score > answer:
+        answer = score
+    return answer
+    if score = playingteams.score()
+        myteam = playingteamname
+    print ("IVE ALWAYS BEEN A " myteam " SUPPORTER")
+    print ("THEY ARE MY LOCAL FROM " myteam.firstword)
+    print ( playingteam.random " ARE TRASH CAUSE IM SCARED OF " ['Western Sydney', 'Socially Responsible Policy', 'Progressive Thinking', 'Gun Control'].random)
+
+find_my_team()
